aaaa63acd8f7681b5105d26af12198abaf3e56d8
engnadasm <42079713+engnadasm@users.noreply.github.com>
2020-08-02 01:55:53 +0200

    navigation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,72 +5,86 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Dropdown } from 'react-css-dropdown';
 import SignUp from './SignUp';
 import Login from './Login';
+import Home from './Home';
+import Footer from './Footer';
 
 class NavBar extends Component {
     constructor(props) {
+
         super()
         this.state = {
             // Takes active tab from props if it is defined there
-            activeTab: props.activeTab || 1,
-            activeTab2: props.activeTab2 || 1
-
+            activeDropMenu:  1,
+            navTap: 1,
+            M : Home
         };
-        // Bind the handleSelect function already here (not in the render function)
-        this.handleSelect = this.handleSelect.bind(this);
-        this.handleSelect2 = this.handleSelect2.bind(this);
+        // Bind the dropdownSelect function already here (not in the render function)
+        this.dropdownSelect = this.dropdownSelect.bind(this);
+        this.navSelect = this.navSelect.bind(this);
 
     }
 
-    handleSelect(selectedTab) {
-        // The active tab must be set into the state so that
-        // the Tabs component knows about the change and re-renders.
+    dropdownSelect(selectedTab) {
         this.setState({
-            activeTab: selectedTab
+            activeDropMenu: selectedTab
         });
         alert("You prefer browsing internet with " + selectedTab)
     };
 
-    handleSelect2(selectedTab) {
-        // The active tab must be set into the state so that
-        // the Tabs component knows about the change and re-renders.
+    navSelect(selectedTab) {
         this.setState({
-            activeTab2: selectedTab
+            navTap: selectedTab
         });
-        if(selectedTab== 3){
-          //alert("You prefer browsing internet with " + selectedTab);
-          window.location.href = SignUp;
-          this.props.navigation.navigate('SignUp')
 
+        if(selectedTab == 1){
+          this.setState({
+              M : Home
+          });
+        }else if(selectedTab == 2){
+          this.setState({
+              M : Login
+          });
+        }else if(selectedTab == 3){
+          this.setState({
+              M : SignUp
+          });
         }
+
     };
     render() {
         return (
+          <React.Fragment>
             <Navbar fill variant="tabs" collapseOnSelect expand="lg" bg="dark" variant="dark"
-            activeKey={this.state.activeTab2} onSelect={this.handleSelect2}>
+            activeKey={this.state.navTap} onSelect={this.navSelect}>
                 <Navbar.Brand href="#home" >Alex Mall Logo</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <Nav.Link class="active" href="#home" eventKey={1} title="Tab 1"
-                        ><i class="fa fa-fw fa-home" eventKey={1} title="Tab 1"></i> Home</Nav.Link>
-                        <NavDropdown  class="fa fa-fw fa-book" activeKey={this.state.activeTab} onSelect={this.handleSelect}
+                        ><i class="fa fa-fw fa-home" ></i> Home</Nav.Link>
+                        <NavDropdown  class="fa fa-fw fa-book" activeKey={this.state.activeDropMenu} onSelect={this.dropdownSelect}
                             title="Categories" type="hj" id="browsers2" href="#Categories-dropdown">
                             <NavDropdown.Item  eventKey={1} title="Tab 1" href="#action/4.1" >Shoes</NavDropdown.Item>
                             <NavDropdown.Item eventKey={2} title="Tab 2" href="#action/4.2" >Kids</NavDropdown.Item>
                             <NavDropdown.Item  eventKey={3} title="Tab 3" href="#action/4.3">Women</NavDropdown.Item>
                             <NavDropdown.Item eventKey={4} title="Tab 4" href="#action/4.4">Men</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/4.5">All</NavDropdown.Item>
+                            <NavDropdown.Item eventKey={5} title="Tab 5" href="#action/4.5">All</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                     <Nav inline>
                         <Nav.Link href="#Login" eventKey={2} title="Tab 2"
-                        ><i class="fa fa-fw fa-sign-in" eventKey={2} title="Tab 2"></i> Login</Nav.Link>
+                        ><i class="fa fa-fw fa-sign-in" ></i> Login</Nav.Link>
                         <Nav.Link href="#SignUp" eventKey={3} title="Tab 3"
-                        ><i class="fa fa-fw fa-user" eventKey={3} title="Tab 3"></i> SignUp</Nav.Link>
+                        ><i class="fa fa-fw fa-user" ></i> SignUp</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
+            <React.StrictMode>
+            <this.state.M/>
+            <Footer/>
+            </React.StrictMode>
+            </React.Fragment>
 
         );
     }
